27851b08ccbafbb1fbe6b3672d4dbdae33ae93f5
Chen Xiangyu <chenxy@beiqicloud.com>
2025-07-15 14:19:38 +0800

    改造成库调用 android上测试正常

--- Android.mk
+++ Android.mk
@@ -1,8 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
  
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := uart-test.cpp libuart.cpp utils.cpp
-LOCAL_SHARED_LIBRARIES := liblog libm libc libprocessgroup libcutils libutils
+LOCAL_SRC_FILES := libuart.cpp utils.cpp
+LOCAL_CFLAGS := -Werror -DDEBUG=1
+LOCAL_MODULE := libuart
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := uart-test.cpp
+LOCAL_SHARED_LIBRARIES := liblog libm libc libprocessgroup libcutils libutils libuart
 LOCAL_CFLAGS := -Werror -DDEBUG=1
 LOCAL_CPPFLAGS += -fexceptions
 LOCAL_MODULE := uart-test
