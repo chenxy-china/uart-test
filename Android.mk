770adfd5a7f5f035153f67a93e07b19aeeb635b8
Chen Xiangyu <chenxy@beiqicloud.com>
2025-07-16 10:38:51 +0800

    修改打包成lib库的设置

--- Android.mk
+++ Android.mk
@@ -1,17 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
  
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libuart.cpp utils.cpp
+LOCAL_SRC_FILES := libbquart.cpp libuart.cpp utils.cpp
 LOCAL_CFLAGS := -Werror -DDEBUG=1
 LOCAL_MODULE := libuart
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libbquart.cpp
-LOCAL_SHARED_LIBRARIES := liblog libm libc libprocessgroup libcutils libutils libuart
-LOCAL_CFLAGS := -Werror -DDEBUG=1
-LOCAL_MODULE := libbquart
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/
 include $(BUILD_SHARED_LIBRARY)
 
